a0b6bfed841ae999f254e6c98f06c973acb5582a
svgen83 <61458549+svgen83@users.noreply.github.com>
2022-02-25 12:34:49 +0400

    исправлены опечатки, регистры букв

--- cartoons.py
+++ cartoons.py
@@ -19,7 +19,8 @@ def get_number_file(url):
     min_number_file = 1
     max_number_file = response.json()["num"]
     return randint(min_number_file, max_number_file)
-    
+
+
 def handle_vk_response(response):
     try:
         response.raise_for_status()
@@ -30,7 +31,8 @@ def handle_vk_response(response):
         print("error code:", response.json()['error']['error_code'])
         print(response.json()['error']['error_msg'])
         os._exit(0)
-        
+
+
 def get_vk_upload_urL(vk_endpoint_template, vk_params):
     response = requests.get(
         vk_endpoint_template.format("photos.getWallUploadServer"),
@@ -51,12 +53,12 @@ def upload_file(vk_upload_url, vk_params, file_name):
 
 
 def get_issue_params(vk_access_token, group_id, save_params, json_comics_response):
-    saveWall_response = requests.post(
-        vk_endpoint_template.format("photos.saveWallPhoto"),
+    savewall_response = requests.post(
+        vk_endpoint_template.format("photos.savewallPhoto"),
         params=save_params)
-    saveWall_stuff = handle_vk_response(saveWall_response)
-    owner_id = str(saveWall_stuff["response"][0]["owner_id"])
-    media_id = str(saveWall_stuff["response"][0]["id"])
+    savewall_stuff = handle_vk_response(savewall_response)
+    owner_id = str(savewall_stuff["response"][0]["owner_id"])
+    media_id = str(savewall_stuff["response"][0]["id"])
     description = json_comics_response["alt"]
     title = json_comics_response["title"]
     return {
@@ -71,9 +73,9 @@ def get_issue_params(vk_access_token, group_id, save_params, json_comics_respons
 
 
 if __name__ == "__main__":
-    
+
     load_dotenv()
-    
+
     client_id = os.getenv("CLIENT_ID")
     vk_access_token = os.getenv("VK_ACCESS_TOKEN")
     group_id = os.getenv("GROUP_ID")
@@ -89,7 +91,7 @@ if __name__ == "__main__":
     }
 
     comics_url = "https://xkcd.com/{}/info.0.json".format(number_loading_file)
-    
+
     comics_response = requests.get(comics_url, params=None)
     comics_response.raise_for_status()
     json_comics_response = comics_response.json()
@@ -99,11 +101,10 @@ if __name__ == "__main__":
 
     vk_upload_url = get_vk_upload_urL(vk_endpoint_template, vk_params)
     save_params = upload_file(vk_upload_url, vk_params, file_name)
-    
+
     issue_params = get_issue_params(vk_access_token, group_id, save_params, json_comics_response)
-    
+
     issue_response = requests.post(vk_endpoint_template.format("wall.post"), params=issue_params)
     handle_vk_response(issue_response)
 
     os.remove(file_name)
-
