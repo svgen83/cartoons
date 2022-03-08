3197e458e861dfdd08650510a7b0401e123d27cf
svgen83 <61458549+svgen83@users.noreply.github.com>
2022-03-08 16:33:27 +0400

    исправлена функция hanldle_vk_response, изменены кавычки, названия переменных

--- cartoons.py
+++ cartoons.py
@@ -32,8 +32,11 @@ def get_json_comics_response(number_loading_file):
 def handle_vk_response(response):
     response.raise_for_status()
     json_stuff = response.json()
-    if 'error' in json_stuff:
-    raise requests.HTTPError
+    if "error" in json_stuff:
+        error_msg = dedent(f"""error code:{json_stuff["error"]["error_code"]}.
+                    {json_stuff["error"]["error_msg"]}""")
+        print(error_msg)
+        raise requests.HTTPError
     return json_stuff
 
 
@@ -47,39 +50,41 @@ def get_vk_upload_urL(vk_params):
 
 
 def upload_file(vk_upload_url, vk_params, file_name):
-    with open(file_name, 'rb') as file:
+    with open(file_name, "rb") as file:
         files = {"photo": file}
-        uploaded_file_response = requests.post(vk_upload_url,
-                                               files=files,
-                                               params=vk_params)
-    save_params = handle_vk_response(uploaded_file_response)
+        response = requests.post(vk_upload_url,
+                                 files=files,
+                                 params=vk_params)
+    save_params = handle_vk_response(response)
     save_params.update(vk_params)
     return save_params
 
 
 def get_issue_params(vk_access_token, group_id, save_params, json_comics_response):
     vk_endpoint_template = "https://api.vk.com/method/{}"
-    savewall_response = requests.post(
+    response = requests.post(
         vk_endpoint_template.format("photos.saveWallPhoto"),
         params=save_params)
-    savewall_stuff = handle_vk_response(savewall_response)
-    owner_id = savewall_stuff["response"][0]["owner_id"]
-    media_id = savewall_stuff["response"][0]["id"]
+    response.raise_for_status()
+    json_stuff = handle_vk_response(response)
+    owner_id = json_stuff["response"][0]["owner_id"]
+    media_id = json_stuff["response"][0]["id"]
     description = json_comics_response["alt"]
     title = json_comics_response["title"]
     return {
-        'access_token': vk_access_token,
-        'v': "5.131",
-        "owner_id": f"-{group_id}",
-        "from_group": "1",
-        "attachments": f"photo{owner_id}_{media_id}",
-        "message": dedent(f"{title}\n{description}")}
+            "access_token": vk_access_token,
+            "v": "5.131",
+		    "owner_id": f"-{group_id}",
+		    "from_group": "1",
+		    "attachments": f"photo{owner_id}_{media_id}",
+		    "message": dedent(f"{title}\n{description}")}
         
         
 def publishing_comics(publishing_params):
     vk_endpoint_template = "https://api.vk.com/method/{}"
-    publishing_response = requests.post(vk_endpoint_template.format("wall.post"), params=issue_params)
-    handle_vk_response(publishing_response)
+    response = requests.post(vk_endpoint_template.format("wall.post"), params=publishing_params)
+    response.raise_for_status()
+    json_stuff = handle_vk_response(response)
 
 
 if __name__ == "__main__":
@@ -89,12 +94,13 @@ if __name__ == "__main__":
     client_id = os.getenv("CLIENT_ID")
     vk_access_token = os.getenv("VK_ACCESS_TOKEN")
     group_id = os.getenv("GROUP_ID")
-
+    
+    current_comics_url = "https://xkcd.com/info.0.json"
     file_name = "comics.jpg"
 
     vk_params = {
-        'access_token': vk_access_token,
-        'v': "5.131",
+        "access_token": vk_access_token,
+        "v": "5.131",
         "group_id": group_id
     }
 
@@ -106,9 +112,8 @@ if __name__ == "__main__":
         vk_upload_url = get_vk_upload_urL(vk_params)
         save_params = upload_file(vk_upload_url, vk_params, file_name)
         publishing_params = get_issue_params(vk_access_token, group_id, save_params, json_comics_response)
-        publishing_comics(issue_params)
+        publishing_comics(publishing_params)
     except requests.HTTPError:
-        print("error code:", json_stuff['error']['error_code'])
-        print(json_stuff['error']['error_msg'])
+        pass
     finally:
         os.remove(file_name)
